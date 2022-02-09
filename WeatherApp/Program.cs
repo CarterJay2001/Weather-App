f381917969e8f679998007e5259af93b8718633b
Jaylon Carter <carterjay2001@outlook.com>
2022-02-09 14:04:10 -0600

    initial commit

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string APIKey = JObject.Parse(File.ReadAllText("appsettings.json")).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Welcome to Carter Weather.\n");
+            Console.WriteLine("Please enter your ZipCode:\n");
+            var zip = Console.ReadLine();
+
+            string apiCall = $"http://api.openweathermap.org/data/2.5/weather?zip={zip},us&units=imperial&appid={APIKey}";
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} in your zipcode of: {zip}");
+        }
+    }
+}
---
+++ WeatherApp/WeatherMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            return double.Parse(JObject.Parse(client.GetStringAsync(apiCall).Result)["main"]["temp"].ToString());
+
+        }
+    }
+}
